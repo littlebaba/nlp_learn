7f839a2aaa4be13ca81562604283f0d477eac8f8
liheng <453984016@qq.com>
2023-09-10 21:49:08 +0800

    句子数据分批

--- data/imdb_data.py
+++ data/imdb_data.py
@@ -1,5 +1,7 @@
 import os
 import re
+import time
+from concurrent.futures.thread import ThreadPoolExecutor
 
 import numpy as np
 
@@ -60,12 +62,15 @@ def process_sentence(flag):
     '''
     将单词组成的句子映射成单词编号向量，即句子编码
     '''
-    word_list = np.load('npys/word_list')
+    word_list = np.load('npys/word_list.npy')
+    word_list = word_list.tolist()
     data = load_data(path, flag)
     sentence_code = []
-    for d in data:
-        tmp = []
+
+    def task(d):  # 耗时操作
         vec = d[0]
+        tmp = []
+        index = 0
         for v in vec:  # 查每个单词在word_list中的索引，若存在则加入的tmp中，若不存在则按索引399999赋值
             try:
                 index = word_list.index(v)
@@ -80,12 +85,48 @@ def process_sentence(flag):
         else:
             tmp = tmp[:250]
         sentence_code.append(tmp)
+
+    pool = ThreadPoolExecutor(max_workers=6)
+    for d in data:
+        pool.submit(task, d)
+        a = 1
+
+    while len(sentence_code) != len(data):
+        time.sleep(1)
+
     # 转矩阵保存二进制进硬盘
     sentence_code = np.array(sentence_code)
     if flag == 'train':
-        np.save("sentence_code_1", sentence_code)
+        np.save("npys/sentence_code_1", sentence_code)
     else:
-        np.save("sentence_code_1", sentence_code)
+        np.save("npys/sentence_code_2", sentence_code)
 
 
-load_cab_vector('npys/word_list', )
+def proccess_batch(batch_size):
+    '''
+    4万个句子分批，每一批句子的大小为100
+    '''
+    # 加载词表
+    vocabulary_vectors = np.load('npys/vocabulary_vectors.npy')
+    vocabulary_vectors = vocabulary_vectors.tolist()
+    # 加载句子
+    sentence_code_1 = np.load('npys/sentence_code_1.npy')
+    sentence_code_1 = sentence_code_1.tolist()
+    for i in range(25000):
+        sentence_code_1[i] = [vocabulary_vectors[word_idx] for word_idx in sentence_code_1[i]]
+
+    # 加载原始数据
+    train_data = load_data(path)
+    arr_train = []
+    labels_train = []
+    for i in range(1, int(len(train_data) / batch_size) + 1):
+        arr_train.append(sentence_code_1[(i - 1) * batch_size:i * batch_size])  # 加批数据
+        labels_train.append([train_data[k][1] for k in range((i - 1) * batch_size, i * batch_size)])  # 加批标签
+    arr_train = np.array(arr_train)
+    labels_train = np.array(labels_train)
+    np.save('npys/arr_train', arr_train)
+    np.save('npys/labels_train', labels_train)
+    return arr_train, labels_train
+
+
+proccess_batch(100)
